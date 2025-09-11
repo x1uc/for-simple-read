68cb414862cc74f95e5cec3e1bba7b413e8cda17
lixiucheng <lixiucheng@dayukeji.com>
2025-09-11 19:42:27 +0800

    feat: use inject to pass parameters

--- entrypoints/content/demo.vue
+++ entrypoints/content/demo.vue
@@ -1,23 +1,26 @@
 <template>
-    <div class="bg-white border-1 border-gray-300 rounded-xl min-w-80 max-w-120 min-h-20 p-4 text-black">
+    <div class="bg-white border-1 border-gray-300 rounded-xl min-w-100 max-w-120 min-h-20 p-4 text-black">
         <div class="flex justify-between items-center">
             <div class="flex items-center space-x-2">
                 <div class="text-2xl font-bold">{{ ojb.word }}</div>
-                <div class="text-sm bg-gray-200 rounded-sm px-2 py-1">{{ ojb.pronounce }}</div>
+                <div
+                    class="text-sm bg-gray-200 rounded-sm px-2 py-1 hover:bg-green-300 hover:bg-opacity-50 cursor-pointer transition-colors duration-100">
+                    {{ ojb.pronounce }}</div>
             </div>
             <div
                 class="bg-gray-200 rounded px-1 text-base cursor-pointer  hover:bg-orange-500 hover:bg-opacity-50 transition-colors duration-200">
                 ⭐️收藏</div>
         </div>
+        <div class="py-2 text-base text-justify" v-html="ojb.meaning.replace(/\n/g, '<br>')"></div>
     </div>
 </template>
 
 
 <script lang="ts" setup>
-import { ref } from 'vue';
-const ojb = ref({
-    word: 'example',
-    pronounce: '/ɪɡˈzɑːmpəl/',
-    meaning: 'A thing characteristic of its kind or illustrating a general rule.',
-})
+import { inject } from 'vue';
+const ojb = inject('ojb') as {
+    word: string;
+    pronounce: string;
+    meaning: string;
+};
 </script>
--- entrypoints/content/index.ts
+++ entrypoints/content/index.ts
@@ -16,6 +16,12 @@ export default defineContentScript({
           anchor: 'body',
           onMount(container) {
             const app = createApp(demo);
+            app.provide('ojb', {
+              word: 'example',
+              pronounce: '/ɪɡˈzɑːmp(ə)l/',
+              meaning: 
+              "n.例子，例证；榜样，楷模；典型，范例；例句，例题；警戒，受罚（以示警告）者\nv.得到说明，可加以举例说明"
+            });
             app.mount(container);
             return app;
           },
