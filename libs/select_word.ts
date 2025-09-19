60d0a017450e1fff8ab8474e4294214618170d7e
x1uc <ccuix@qq.com>
2025-09-19 21:51:57 +0800

    feat: introduce collection tab on options page

--- libs/select_word.ts
+++ libs/select_word.ts
@@ -1,2 +1,8 @@
 export const select_word_storage = storage.defineItem<string | null>("local:select_word"); 
 
+
+export type WordData = {
+    word: string;
+    pronunciation: string;
+    meaning: string;
+};
